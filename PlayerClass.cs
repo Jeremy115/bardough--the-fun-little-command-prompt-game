d1324bfeee351cf5e374de131b2c25454c25f7b7
Jeremy115 <70788387+Jeremy115@users.noreply.github.com>
2021-02-25 14:24:10 -0600

    Add files via upload

---
+++ PlayerClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PlayerClass
+
+    class player
+    {
+        public string name;
+        public string gender;
+        public int height;
+        public int age;
+        public int weight;
+
+    }
+}
